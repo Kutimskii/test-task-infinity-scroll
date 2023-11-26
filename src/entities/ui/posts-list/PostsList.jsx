f0ff9ce7662fd4877fe7ab05a39c4b4aa2e18d50
kkk <kutimskyi@gmail.com>
2023-11-26 23:00:53 +0300

    second commit

--- src/entities/ui/posts-list/PostsList.jsx
+++ src/entities/ui/posts-list/PostsList.jsx
@@ -11,28 +11,39 @@ export default function PostsList() {
   const { ref: lastCard, inView: inViewLastCard } = useInView({
     threshold: 0.6,
   });
+  const { ref: firstCard, inView: inViewFirstCard } = useInView({
+    threshold: 0.5,
+  });
 
   useEffect(() => {
     if (inViewLastCard) {
-      debugger
       setPostStart((prev) => prev + 1);
     }
   }, [inViewLastCard]);
+
+  useEffect(() => {
+    if (inViewFirstCard) {
+      setPostStart((prev) => prev > 0 ? prev - 1 : prev);
+    }
+  }, [inViewFirstCard]);
 
   return (
     <div>
       <ul className="lists_wrap">
         {data.map((post, index, arr) => {
-  
-            if (index === arr.length - 1 ) {
-              return <li key={post.id} ref={lastCard}>
+          if (index === 0 ) {
+            return <li key={post.id} ref={firstCard}>
                 <PostCard post={post} />
               </li>
-            }else{
-              return <li key={post.id}>
+            }
+          if (index === arr.length -1) {
+            return <li key={post.id} ref={lastCard}>
               <PostCard post={post} />
             </li>
             }
+            return <li key={post.id}>
+              <PostCard post={post} />
+            </li>
         })}
       </ul>
     </div>
--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -5,18 +5,19 @@ import { useNavigate } from "react-router-dom";
 
 export default function PostPage() {
   const { id } = useParams();
-  const { data, isSuccess } = useGetPostByIdQuery(id);
+  const { data, isLoading, isSuccess } = useGetPostByIdQuery(id);
   const navigate = useNavigate();
   const buttonHandler = () => {
     navigate(-1);
   };
+  if (isLoading) return <h1>Loading...</h1>;
   if (!isSuccess) return <h1>Page is not found</h1>;
   return (<>
     <main className ='container'>
       <h1 className = 'post_id'>№ {data.id}</h1>
       <h2 className = 'title'>{data.title}</h2>
       <p>{data.body}</p>
-      <button onClick={buttonHandler}>Назад</button>
+      <button className = 'btn_view' onClick={buttonHandler}>Назад</button>
     </main>
   </>
   );
